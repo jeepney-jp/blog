518b2dc9d8d5239c6d7feabf28d1f0bd5c3ed2d9
jeepney-jp <hidaka@jeepney.co.jp>
2025-08-20 21:59:59 +0900

    fix: index.tsの構文エラーを修正（改行追加）

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,4 +4,6 @@ import serviceDetail from './serviceDetail'
 import contact from '../schemas/contact'
 import staff from './staff'
 import { tableBlock } from './customBlocks/tableBlock'
-import featuresFaq from '../schemas/featuresFaq'export const schemaTypes = [news, serviceCategory, serviceDetail, contact, staff, tableBlock, featuresFaq]
+import featuresFaq from '../schemas/featuresFaq'
+
+export const schemaTypes = [news, serviceCategory, serviceDetail, contact, staff, tableBlock, featuresFaq]
