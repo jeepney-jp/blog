41373557b2547a1c42ace1967558b9792164cbf6
jeepney-jp <hidaka@jeepney.co.jp>
2025-08-20 21:06:01 +0900

    feat: 不要なセクション（お役立ち記事・お客様の声）を削除

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,11 +1,7 @@
 import news from './news'
-import testimonials from './testimonials'
-import blog from './blog'
 import serviceCategory from './serviceCategory'
 import serviceDetail from './serviceDetail'
 import contact from '../schemas/contact'
 import staff from './staff'
 import { tableBlock } from './customBlocks/tableBlock'
-import featuresFaq from '../schemas/featuresFaq'
-
-export const schemaTypes = [news, testimonials, blog, serviceCategory, serviceDetail, contact, staff, tableBlock, featuresFaq]
+import featuresFaq from '../schemas/featuresFaq'export const schemaTypes = [news, serviceCategory, serviceDetail, contact, staff, tableBlock, featuresFaq]
